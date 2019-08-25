8adb7732e85ca67a830cbed9a4d4e2373ea1360d
Anthony David Pulse, Jr <tcp@null.net>
2019-08-24 22:28:00 -0400

    10:27pm 8-24-2019

--- preorders/oxml.xsl
+++ preorders/oxml.xsl
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?xml-stylesheet type="text/xml" version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 	<xsl:output method="html"/>
@@ -20,38 +19,3 @@
 		</xsl:for-each>
 	</xsl:template>
 </xsl:stylesheet>
-=======
-
-<?xml-stylesheet type="text/xml" version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet
- version="1.0"
-xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-xmlns="http://www.w3.org/TR/REC-html40">
-<xsl:output method="html"/>
-<xsl:template match="/">
-    <style>
-    	#td-surround { background:black;border:0px;height:300px;width:250px; }
-    	#in-window { border:2px solid darkblue;overflow-wrap:break-word;overflow-y:scroll;color:black;background:black;height:300px;width:250px; }
-    </style>
-    <table>
-        <tr><td id="td-surround">
-            <xsl:for-each select="preorders/shopper">
-            	<div id="in-window" onclick="menuList('orders.php')" style="font-size:12px;background:black;color:white;width:100%">
-                    <xsl:text>From: </xsl:text>
-               		<xsl:value-of select="from"/>
-                    <xsl:text> - Date: </xsl:text>
-            		<xsl:value-of select="date"/>
-                    <xsl:text> - Products: </xsl:text>
-            		<xsl:value-of select="products"/>
-                    <xsl:text> - Total Items: </xsl:text>
-        			<xsl:value-of select="quantity"/>
-        			<xsl:text> - Contact: </xsl:text>
-            		<xsl:value-of select="email"/>
-       			</div>
-       		</xsl:for-each>
-        </td></tr>
-	</table>
-<xsl:template>
-<xsl:stylesheet>
-	
->>>>>>> 9f1db909a7dc3f1791b4a1d9f5ab17f6cf8001b5
--- preorderxml.php
+++ preorderxml.php
@@ -3,7 +3,6 @@
     srand($_COOKIE['franchise_id']);
     $t = rand(1,465365);
     $salt = rand(1,5);
-<<<<<<< HEAD
     $filename = $salt . md5($_COOKIE['franchise_id'] . $t) . '.xml';
     $dom = "";
     if (!is_dir("preorders/" . $salt . md5($_COOKIE['franchise_id'] . $t))){
@@ -15,20 +14,6 @@
 
     $dom = simplexml_load_file("preorders/" . $salt . md5($_COOKIE['franchise_id'] . $t) . '/' . $filename);
 
-=======
-    $filename = md5($_COOKIE['myemail']) . ".xml";
-    $dom = "";
-    if (!is_dir("preorders/" . $salt . md5($_COOKIE['franchise_id'] . $t))){
-        mkdir("preorders/" . $salt . md5($_COOKIE['franchise_id'] . $t));
-        file_put_contents('preorders/' . $salt . md5($_COOKIE['franchise_id'] . $t) . '/.htaccess', "Require all granted");
-    }
-    if (!file_exists('preorders/' . $salt . md5($_COOKIE['franchise_id'] . $t) . '/' . $filename))       
-        file_put_contents('preorders/' . $salt . md5($_COOKIE['franchise_id'] . $t) . '/' . $filename, "<?xml version='1.0'?><?xml-stylesheet type='text/xsl' href='../oxml.xsl' ?><preorders></preorders>");
-   
-        
-    $dom = simplexml_load_file("preorders/" . $salt . md5($_COOKIE['franchise_id'] . $t) . '/' . $filename);
-
->>>>>>> 9f1db909a7dc3f1791b4a1d9f5ab17f6cf8001b5
     $x = $dom->preorders;
     $y = $_GET['a'];
     $w = $_GET['b'];
@@ -37,11 +22,6 @@
     $b = str_getcsv($w,",");
     $i = 0;
     
-<<<<<<< HEAD
-=======
-    
-
->>>>>>> 9f1db909a7dc3f1791b4a1d9f5ab17f6cf8001b5
     $tmpy = $dom->addChild("items");
     foreach($a as $v) {
         $tmpy->addChild("product", $v);
@@ -50,7 +30,6 @@
         $tmpy->addAttribute("from", $_COOKIE['myname']);
         $tmpy->addAttribute("date", date('d-m-Y',time()));
         $i++;
-<<<<<<< HEAD
         $dom->asXML('preorders/' . $salt . md5($_COOKIE['franchise_id'] . $t) . '/' . $filename);
     }
     
@@ -67,34 +46,13 @@
 
     $x = $dom2->preorders;
     
-=======
-        echo $dom->asXML('preorders/' . $salt . md5($_COOKIE['franchise_id'] . $t) . '/' . $filename);
-    }
-    
-    if (!is_dir("inbox/" . $salt . md5($_COOKIE['franchise_id'] . $t))) {
-        mkdir("inbox/" . $salt . md5($_COOKIE['franchise_id'] . $t));
-        file_put_contents('inbox/' . $salt . md5($_COOKIE['franchise_id'] . $t) . '/.htaccess', "Require all granted");
-    }
-    if (!file_exists('inbox/' . $salt . md5($_COOKIE['franchise_id'] . $t) . '/' . $filename))       
-        file_put_contents('inbox/' . $salt . md5($_COOKIE['franchise_id'] . $t) . '/' . $filename, "<?xml version='1.0'?><?xml-stylesheet type='text/xsl' href='../oxml.xsl' ?><preorders></preorders>");
-
-    $dom2 = simplexml_load_file('inbox/' . $salt . md5($_COOKIE['franchise_id'] . $t) . '/' . $filename);
-
-    $x = $dom2->preorders;
-
->>>>>>> 9f1db909a7dc3f1791b4a1d9f5ab17f6cf8001b5
     $tmpy = $dom2->addChild("shopper");
     $tmpy->addChild("products", count($a));
     $tmpy->addChild("items", count($b));
     $tmpy->addChild("email", $_COOKIE['myemail']);
     $tmpy->addChild("from", $_COOKIE['myname']);
     $tmpy->addChild("date", date('d-m-Y',time()));
-<<<<<<< HEAD
 
     $dom2->asXML('inbox/' . $filename);
-=======
-    
-    echo $dom2->asXML('inbox/' . $salt . md5($_COOKIE['franchise_id'] . $t) . '/' . $filename);
->>>>>>> 9f1db909a7dc3f1791b4a1d9f5ab17f6cf8001b5
 
 ?>
